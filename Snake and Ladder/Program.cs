e839d4ae59374a310d992f7542b0befabaaa8822
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-09 17:04:00 +0530

    UC2-dice roll

--- Snake and Ladder/Program.cs
+++ Snake and Ladder/Program.cs
@@ -7,13 +7,14 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-
+            Random random = new Random();
+            int Diceroll = random.Next(1, 7);
 
 
             int position = 0;
-            Console.WriteLine("Welcome to Snake and Ladder");
-
+            Console.WriteLine("Welcome to Snake and Ladder");         
             Console.WriteLine("initial position is " + position);
+            Console.WriteLine("Dice roll : " + Diceroll);
         }
     }
 }
