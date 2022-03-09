bd417413744ea8dc8c5ae7f96e40aa77b303eb94
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-09 22:48:00 +0530

    UC3-position of player is changing

--- Snake and Ladder/Program.cs
+++ Snake and Ladder/Program.cs
@@ -7,14 +7,35 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            int position = 0;
             Random random = new Random();
             int Diceroll = random.Next(1, 7);
-
-
-            int position = 0;
-            Console.WriteLine("Welcome to Snake and Ladder");         
+            Random checkOption = new Random();
+            int Options = random.Next(0, 3);
             Console.WriteLine("initial position is " + position);
             Console.WriteLine("Dice roll : " + Diceroll);
+
+            switch (Options)
+            {
+                case 0:
+                    position = position;
+                    Console.WriteLine("player remains at = 0");
+                    break;
+
+                case 1:
+                    position = position + Diceroll;
+                    Console.WriteLine("player movers ahead =" +Diceroll);
+                    break;
+
+
+                case 3:
+                    position = position - Diceroll;
+                    Console.WriteLine("player moves back =" + Diceroll );
+                    break;
+
+            }
+            
+
         }
     }
 }
