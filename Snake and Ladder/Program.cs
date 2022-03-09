82a67320e954ac9d700a46b1cb42e0c132d9013b
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-09 14:32:26 +0530

    Added UC1-inital position is 0

--- Snake and Ladder/Program.cs
+++ Snake and Ladder/Program.cs
@@ -1,3 +1,19 @@
-﻿
+﻿using System;
 
-Console.WriteLine("Welcome to Snake and Ladder");
+namespace SnakeAndLadder
+
+{
+    class Snakeandladder
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int position = 0;
+            Console.WriteLine("Welcome to Snake and Ladder");
+
+            Console.WriteLine("initial position is " + position);
+        }
+    }
+}
